4bb7915585e493df6a8a7e9f2964e897debbeff3
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2023-08-12 11:26:11 -0700

    Intro to Express Part 2

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
 
 app.use((req, res) => {
     console.log(req.headers);
